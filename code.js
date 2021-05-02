bd84acd00ba577b814aa29d219302b08ca0ed8b6
dobozit <dobozi111@gmail.com>
2021-05-02 17:00:46 +0200

    hibamentes

--- code.js
+++ code.js
@@ -5,17 +5,14 @@ const closeMenu = document.querySelector('.closeMenu')
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
-function show(ev) {
+function show(){
     mainMenu.style.display = "flex";
-    mainMenu.style.display = '0';
-
-    ev.preventDefault();
+    mainMenu.style.top = '0';
+    openMenu.preventDefault();
     
 }
 
-function close(ev) {
+function close(){
     mainMenu.style.top = '-100vh';
-    ev.preventDefault();
-   
-
+    closeMenu.preventDefault();
 }
